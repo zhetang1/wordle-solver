d1aeb57c842bf7ef5c7e3f6f15e927b6ba530b7f
Zhe Tang <zhetang1@gmail.com>
2022-02-11 22:37:33 -0500

    Rewrite the structure of filter so it can handle the case when one char appears multiple times.

--- components/dynamicTable.js
+++ components/dynamicTable.js
@@ -12,7 +12,6 @@ export default class DynamicTable extends React.Component {
         import(/*webpackIgnore: true*/ 'https://assets.codepen.io/1581715/fivewords.js')
             .then( result => {
                      const state = {
-                       message: "",
                        items: [
                          ["", "", "", "", ""],
                          ["", "", "", "", ""],
@@ -23,11 +22,9 @@ export default class DynamicTable extends React.Component {
                        ],
                        row: 0,
                        column: 0,
-//                       allWords: testWords,
-//                       filteredWords: testWords,
-                      allWords: result.allWords,
-                      filteredWords: result.allWords,
-                       filters: {}
+//                       allWords: testWords, filteredWords: testWords,
+                       allWords: result.allWords, filteredWords: result.allWords,
+                       filters: []
                      };
                      this.setState(state);
                   }, function(error) {
@@ -48,7 +45,6 @@ export default class DynamicTable extends React.Component {
 
     this.setState({
         items: items,
-        message: "",
         row: row,
         column: column
     });
@@ -62,7 +58,6 @@ export default class DynamicTable extends React.Component {
         row++;
         this.setState({
           items: this.state.items,
-          message: "",
           row: row,
           column: 0
         });
@@ -82,7 +77,6 @@ export default class DynamicTable extends React.Component {
 
         this.setState({
             items: items,
-            message: "",
             row: row,
             column: column
         });
@@ -91,36 +85,37 @@ export default class DynamicTable extends React.Component {
     filterWords(status, char, position) {
         const state = this.state;
         const filters = state.filters;
-        filters[char] = {status: status, position: position};
+        // filters: [{char: "A", status: "hit}, {char: "B", status: "no hit"}, {char: "C", status: "wrong position"}]
+
+        filters[position] = {char: char, status: status};
         const words = [];
 
         this.state.allWords.map( word => {
             let include = true;
 
-             for (const key in filters) {
-                if(key !== "") {
-                    const c = key.toLowerCase();
-                    const p = filters[key]["position"];
-                    switch (filters[key]["status"]){
-                        case "":
-                            break;
-                        case "no hit":
-                            if (word.includes(c)) include = false;
-                            break;
-                        case "wrong position":
-                            if (!word.includes(c)) include = false;
-                            if (word[p] === c) include = false;
-                            break;
-                        case "hit":
-                            if (word[p] !== c) include = false;
-                            break;
-                        default:
-                            alert('Default case');
-                    }
-                }
-             }
+            for (var i=0; i<filters.length; i++) {
+                const c = filters[i].char.toLowerCase();
+                const p = i % 5;
 
-             if (include) words.push(word);
+                switch (filters[i].status){
+                    case "":
+                        break;
+                    case "no hit":
+                        if (word.includes(c)) include = false;
+                        break;
+                    case "wrong position":
+                        if (!word.includes(c)) include = false;
+                        if (word[p] === c) include = false;
+                        break;
+                    case "hit":
+                        if (word[p] !== c) include = false;
+                        break;
+                    default:
+                        alert('Default case');
+                }
+            }
+
+            if (include) words.push(word);
         });
 
         state.filters = filters;
@@ -129,7 +124,7 @@ export default class DynamicTable extends React.Component {
     }
 
     renderRows() {
-        const eventhandler = ({status, char, position}) => {
+        const eventHandler = ({status, char, position}) => {
             this.filterWords(status, char, position);
         }
 
@@ -137,19 +132,19 @@ export default class DynamicTable extends React.Component {
             return (
                 <tr key={"row-" + i}>
                     <td key="column-0">
-                        <Square char={o[0]} onChange={eventhandler} position={0} />
+                        <Square char={o[0]} onChange={eventHandler} position={i*5 + 0} />
                     </td>
                     <td key="column-1">
-                        <Square char={o[1]} onChange={eventhandler} position={1} />
+                        <Square char={o[1]} onChange={eventHandler} position={i*5 + 1} />
                     </td>
                     <td key="column-2">
-                        <Square char={o[2]} onChange={eventhandler} position={2} />
+                        <Square char={o[2]} onChange={eventHandler} position={i*5 + 2} />
                     </td>
                     <td key="column-3">
-                        <Square char={o[3]} onChange={eventhandler} position={3} />
+                        <Square char={o[3]} onChange={eventHandler} position={i*5 + 3} />
                     </td>
                     <td key="column-4">
-                        <Square char={o[4]} onChange={eventhandler} position={4} />
+                        <Square char={o[4]} onChange={eventHandler} position={i*5 + 4} />
                     </td>
                 </tr>
             );
